83b49a27c32d37bed1aaf92d0a11fe1705aa0082
Chamane Barbatti <chamane@sbcglobal.net>
2022-10-27 02:16:04 -0500

    Add project files.

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEvents.Controllers
+{
+
+    public class EventsController : Controller
+    {
+         private static readonly Dictionary<string,string> Events = new Dictionary<string, string>();
+        [HttpGet]
+        public IActionResult Index()
+        {
+           
+            
+            ViewBag.events = Events;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("/Events/Add")]
+        public IActionResult NewEvent(string name, string desc)
+        {
+            Events.Add(name, desc);
+            return Redirect("/Events");
+        }
+    }
+}
