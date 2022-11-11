88710ead0db193c2cba75d9f5bfdb253a1258fb4
Chamane Barbatti <chamane@sbcglobal.net>
2022-11-11 12:51:26 -0600

    working getting migration to work

--- CodingEvents/Models/Event.cs
+++ CodingEvents/Models/Event.cs
@@ -12,21 +12,19 @@ namespace CodingEvents.Models
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public int Id { get; }
-         static private int nextId = 1;
+        public int Id { get; set; }
+        
        
 
         public Event(string name, string description)
         {
             Name = name;
             Description = description;
-            Id=nextId;
-            nextId++;
+            
         }
         public Event()
         {
-            Id = nextId;
-            nextId++;
+           
 
         }
         public override string ToString()
